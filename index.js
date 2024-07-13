750a0d46595490c74349ce89933529ef69c1a277
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-14 00:03:15 +0100

    dot env configured

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
