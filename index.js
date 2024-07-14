bc82dec295d6621642fcf038a8ea49abbdf1f4d9
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-14 14:33:42 +0100

    Mongo db connected

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const contactRoutes = require('./routes/contactRoutes');
 const errorHander = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 3000;
