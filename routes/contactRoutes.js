a6805d52a9241b74857d563de0ca4110fc4c9874
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-14 08:53:07 +0100

    Routes simplified

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,10 +8,13 @@ const {
   deleteContact,
 } = require('../controllers/contactController');
 
-router.route('/').get(getContacts);
-router.route('/:id').get(getContactById);
-router.route('/').post(createContact);
-router.route('/:id').put(updateContact);
-router.route('/:id').delete(deleteContact);
+// router.route('/').get(getContacts);
+// router.route('/:id').get(getContactById);
+// router.route('/').post(createContact);
+// router.route('/:id').put(updateContact);
+// router.route('/:id').delete(deleteContact);
 
+// Routes simplified
+router.route('/').get(getContacts).post(createContact);
+router.route('/:id').get(getContactById).put(updateContact).delete(deleteContact);
 module.exports = router;
