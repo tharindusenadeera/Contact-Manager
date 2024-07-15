d9cc80f141241ff72b6ced18ce87d1f0cd39f495
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-15 21:47:19 +0100

    Access token creation done using jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 //@desc current user
@@ -50,12 +51,29 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access public
 
 const loginUser = asyncHandler(async (req, res) => {
-  const { username, password } = req?.body;
-  if (!username || !password) {
+  const { email, password } = req?.body;
+  if (!email || !password) {
     res.status(400);
-    throw new Error('Enter a valid username and password!');
+    throw new Error('All fields are mandatory!');
   }
-  res.status(201).json({ message: 'User logged in' });
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user?.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user?.username,
+          email: user?.email,
+          id: user?._id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '1m' }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error('Email or password not valid!');
+  }
 });
 
 module.exports = {
