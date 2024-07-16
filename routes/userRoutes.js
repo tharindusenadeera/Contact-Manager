7b5d950afc8e173c2e7d3958b8a954d48e8cbd63
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-16 17:00:24 +0100

    Token validation middleware added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { loginUser, registerUser, getCurrentUser } = require('../controllers/userController');
+const validateToken = require('../middleware/validateTokenHandler');
 
 const router = express.Router();
 
-router.route('/current').get(getCurrentUser);
+router.route('/current').get(validateToken, getCurrentUser);
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 
